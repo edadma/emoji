c90777f06f349ec1f7d316310e2756851efafa0b
Edward A Maxedon Sr <edadma@gmail.com>
2021-11-08 20:49:50 -0500

    code emoji translator

--- shared/src/main/scala/io/github/edadma/emoji/Main.scala
+++ shared/src/main/scala/io/github/edadma/emoji/Main.scala
@@ -2,9 +2,6 @@ package io.github.edadma.emoji
 
 object Main extends App {
 
-  println("""
-      |> cross_templateJVM/run
-      |> cross_templateJS/run
-      |> cross_templateNative/run""".trim.stripMargin)
+  println(Emoji(args(0)))
 
 }
